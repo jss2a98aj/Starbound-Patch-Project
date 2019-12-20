93a2e32e67882e38567a744e2eb499489df32ba6
jss2a98aj <jsimmons3959@neo.rr.com>
2019-12-19 19:36:39 -0500

    Partial fix for server <-> client save time ahead of world time bug
    Anything that saves the current world time and is saved on a player ship can have a time in the future saved when traveling between servers or after a universe reset. In vanilla evolving animals, animals that produce items, and plants can suffer from this. Plants appear to use hardcoded functions and cannot be fixed this way.

---
+++ Starbound Patch Project/scripts/actions/monsters/farmable.lua
@@ -0,0 +1,65 @@
+require "/scripts/vec2.lua"
+require "/scripts/poly.lua"
+require "/scripts/util.lua"
+
+-- param entity
+function evolveFluffalo()
+  --Fix spawn time ahead of world time bug
+  if storage.spawnTime > world.time() then
+    storage.spawnTime = world.time()
+    sb.logInfo("SBPP: Fixed farmable monster with spawn time ahead of world time.")
+  end
+  --End fix
+
+  local evolveTime = config.getParameter("evolveTime")
+  if storage.spawnTime + evolveTime < world.time() then
+    local evolveType = config.getParameter("evolveType")
+    local spawnPosition = vec2.add(mcontroller.position(), vec2.mul(config.getParameter("spawnOffset"), {mcontroller.facingDirection(), 1}))
+    if not world.polyCollision(poly.translate(config.getParameter("spawnPoly"), spawnPosition)) then
+      world.spawnMonster(evolveType, spawnPosition, {level = 1, aggressive = false})
+
+      monster.setDropPool(nil)
+      monster.setDeathParticleBurst(nil)
+      monster.setDeathSound(nil)
+      status.setResource("health", 0)
+      return true
+    end
+  end
+  return false
+end
+
+function resetMonsterHarvest()
+  storage.lastHarvest = world.time()
+  storage.harvestTime = util.randomInRange(config.getParameter("harvestTime"))
+end
+
+function hasMonsterHarvest(args, board)
+  if not storage.lastHarvest then
+    resetMonsterHarvest()
+  end
+
+  --Fix last harvest time ahead of world time bug
+  if storage.lastHarvest > world.time() then
+    storage.lastHarvest = world.time()
+    sb.logInfo("SBPP: Fixed farmable monster with last harvest time ahead of world time.")
+  end
+  --End fix
+
+  if world.time() - storage.lastHarvest >= storage.harvestTime then
+    return true
+  else
+    return false
+  end
+end
+
+function dropMonsterHarvest(args, board)
+  local treasurePool = config.getParameter("harvestPool")
+  local treasure = root.createTreasure(treasurePool, monster.level())
+  local spawnPosition = vec2.add(mcontroller.position(), config.getParameter("harvestSpawnOffset", {0, 0}))
+
+  for _,item in pairs(treasure) do
+    world.spawnItem(item, spawnPosition)
+  end
+  resetMonsterHarvest()
+  return true
+end
