b560fb6402e565c1777275b6403fb63259da28ae
jss2a98aj <jss2a98aj@gmail.com>
2022-11-04 05:48:33 -0400

    Kludged the Solus Katana Mech Arm fix even harder
    It now works with unreasonable gravity values, such as 800.

--- Starbound Patch Project/vehicles/modularmech/armscripts/dasharm.lua
+++ Starbound Patch Project/vehicles/modularmech/armscripts/dasharm.lua
@@ -31,19 +31,8 @@ function DashArm:windupState()
 
   local stateTimer = self.windupTime
 
-  -- The vanilla logic will work in zero-G or in low gravity. However,
-  -- it will not work in full gravity environments, because there the
-  -- gravity causes a continuous minimum of ~-1.18 on the y-axis of the
-  -- velocity vector, even when the mech is stationary.
-  local gravity = world.gravity(mcontroller.position())
-  local maxInitialVelocity = 1.0
-  if gravity > 0 and gravity <= 40 then
-    maxInitialVelocity = 1.6
-  elseif gravity > 40 then
-    maxInitialVelocity = 2.2
-  end
-
-  while vec2.mag(mcontroller.velocity()) > maxInitialVelocity or stateTimer > 0 do
+  --SBPP - Allow attacks even with gravity beyond vanilla. Inspired by changes from RL-Starbound.
+  while stateTimer > 0 do
     animator.rotateTransformationGroup(self.armName, self.aimAngle + self.windupAngle, self.shoulderOffset)
     stateTimer = stateTimer - script.updateDt()
     mcontroller.approachVelocity({0.0, 0.0}, self.stopForce)
